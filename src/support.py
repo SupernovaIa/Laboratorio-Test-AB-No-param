1a31308b181806549cbe81d73530fadc17c1be0e
jcarreir <jav.racec@gmail.com>
2024-11-06 18:18:06 +0100

    hecho v2

--- src/support.py
+++ src/support.py
@@ -1,4 +1,6 @@
 from scipy import stats
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def normalidad_ks(df):
 
@@ -24,3 +26,21 @@ def test_no_param(datos, dependencia=False):
 
     else:
         print('Tienes que pasar al menos 2 grupos')
+
+
+def diagrama_barras(df, columna):
+
+    df_ciudades = df.groupby('Loyalty Number')[columna].unique().value_counts().reset_index()
+    df_ciudades[columna] = df_ciudades[columna].apply(lambda x: x[0])
+
+    plt.figure(figsize=(8, 4))
+
+    sns.barplot(x=columna, y='count', data=df_ciudades, palette='mako', edgecolor='black', linewidth=1.5)
+
+    plt.title(f'Personas por {columna}')
+    plt.xlabel('')
+    plt.ylabel('Número de personas')
+
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
