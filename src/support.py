9a3a03e0b99bc67823edd5ccac78dc0a572c3e19
jcarreir <jav.racec@gmail.com>
2024-11-06 17:08:26 +0100

    hecho v1

--- src/support.py
+++ src/support.py
@@ -6,3 +6,21 @@ def normalidad_ks(df):
     data_std = df.std()
     
     return stats.kstest(df, 'norm', args=(data_mean, data_std))
+
+
+def test_no_param(datos, dependencia=False):
+
+    if len(datos) > 2:
+        print('Test Kruskal Wallis')
+        return stats.kruskal(*datos)
+
+    elif len(datos) == 2 and dependencia:
+        print('Wilcoxon')
+        return stats.wilcoxon(*datos)
+
+    elif len(datos) == 2 and not dependencia:
+        print('Test U')
+        return stats.mannwhitneyu(*datos)
+
+    else:
+        print('Tienes que pasar al menos 2 grupos')
